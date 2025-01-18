31ad5d5f5a26dc6df1572e8238c6e20a9ef73ae6
Gad Nadjar <gadnadjar@gmail.com>
2025-01-18 11:37:58 +0200

    finish navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
 import logo from '../assets/nature-logo.png';
 
-type Props = {}
+const LoginMenu = [
+    {
+        id: 1,
+        link: '#',
+        name: 'Home'
+    },
+    {
+        id: 2,
+        link: '#',
+        name: 'About'
+    },
+    {
+        id: 3,
+        link: '#',
+        name: 'Contact'
+    },
+    {
+        id: 4,
+        link: '#',
+        name: 'Login'
+    }
+];
 
-const NavBar = (props: Props) => {
+const NavBar = () => {
     return (
         <header className='bg-white/80 shadow-lg after:'>
-            <div className='container flex items-center justify-between pt-2 pb-2'>
+            <div className='container flex items-center justify-between'>
                 <nav>
                     <a href="#" className='text-gray-800 font-bold text-3xl'>
                         <img className='h-10 mr-1 inline' src={logo} />
@@ -15,20 +35,27 @@ const NavBar = (props: Props) => {
                 </nav>
 
                 {/* Desktop Menu  */}
-                <div>
-                    <ul className='flex gap-10 text-2xl'>
-                        <li>
-                            <a className='hidden sm:block' href="#">Home</a>
-                        </li>
-                        <li>
-                            <a className='hidden sm:block' href="#">About</a>
-                        </li>
-                        <li>
-                            <a className='hidden sm:block' href="#">Contact</a>
-                        </li>
-                        <li>
-                            <a href="#">Login</a>
-                        </li>
+                <div className='sm:block'>
+                    <ul className='flex gap-4 text-2xl justify-center items-center'>
+                        {
+                            LoginMenu.map((item) => (
+                                <li className='text-gray-700 hover:text-gray-900 px-4 py-4 inline-block select-none cursor-pointer' key={item.id}>
+                                    <a href={item.link}></a>
+                                    {
+                                        item.name === 'Login' && (
+                                            <li>{item.name}</li>
+                                        )
+                                    }
+
+                                    {
+                                        item.name !== 'Login' && (
+                                            <li className='hidden sm:block'>{item.name}</li>
+                                        )
+
+                                    }
+                                </li>
+                            ))
+                        }
                     </ul>
                 </div>
 
